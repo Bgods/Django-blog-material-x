32fc4c0f5c3324b0e39aa0697b51c1354dad65bb
Bgods <932794753@qq.com>
2019-04-06 15:39:53 +0800

    改用sqlite3

--- www/www/settings.py
+++ www/www/settings.py
@@ -79,17 +79,13 @@ WSGI_APPLICATION = 'www.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'databasename',
-        'USER': '****',
-        'PASSWORD': '****',
-        'PORT': '3306',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
