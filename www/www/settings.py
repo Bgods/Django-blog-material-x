05f880a8386daf491f2339b6bae467773999cbe8
Bgods <932794753@qq.com>
2020-07-07 15:24:10 +0800

    Update settings.py

--- www/www/settings.py
+++ www/www/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "collect_static")
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "common_static"),
+   os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = '/media/'
